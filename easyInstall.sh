37089ff33b7fb935030ce158f31f2dfbd9359e2b
fifthPrince <thirtyfive16@126.com>
2017-06-26 21:57:23 +0800

    Update easyInstall.sh

--- easyInstall.sh
+++ easyInstall.sh
@@ -12,4 +12,12 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda install numpy scipy mkl
 conda install theano 
 
+##pip install keras==1.2.2
 pip install keras
+
+echo '{
+    "image_dim_ordering": "th",
+    "epsilon": 1e-07,
+    "floatx": "float32",
+    "backend": "theano"
+}' > ~/.keras/keras.json
